9eb92b122ebfcee497f5f83a7885bd206dfe7a23
fuk3t <bradwilliams1989@yahoo.com>
2018-04-28 12:20:04 -0400

    Added username to logout function

--- nav.php
+++ nav.php
@@ -1,6 +1,8 @@
 <?php
 /* Central file for site navigation */
 
+$userNav = SessionManager::getUserName();
+
 ?>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
@@ -30,7 +32,7 @@
                         <a class="nav-link" href="/account.php">Account</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/logout.php">LogOut</a>
+                        <a class="nav-link" href="/logout.php">Log Out, &nbsp; <?php echo "<p{display: inline;}>" . $userNav . "</p>" ?></a>
                     </li>
                 <?php else: ?>
                     <li class="nav-item">
