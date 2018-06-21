84af1ac5b11fe87ead9b135b796781e190148bbc
h0r53 <jjm15e@my.fsu.edu>
2018-06-21 19:45:22 -0400

    Updated footer

--- footer.php
+++ footer.php
@@ -13,6 +13,19 @@
             <li class="list-inline-item">
               <iframe allowtransparency="true" scrolling="no" frameborder="no" src="https://w.soundcloud.com/icon/?url=http%3A%2F%2Fsoundcloud.com%2Ftheambientfunk&color=orange_white&size=64" style="width: 64px; height: 64px;"></iframe>
             </li>
+			<li class="list-inline-item" style="vertical-align:top;">
+
+			<img src="images/solidink.png" style="max-height:64px;"></img>
+            </li>
+			<li class="list-inline-item" style="vertical-align:top;">
+			  <a href="https://www.opendevtools.org/dalgen.php">
+			    <img src="images/dalgen.png" style="max-height:64px;"></img>
+			  </a>
+            </li>
+			<li class="list-inline-item" style="vertical-align:top;">
+
+			<img src="images/Nginx_Logo.png" style="max-height:64px;"></img>
+            </li>
         </ul>
         <p class="copyright text-muted small">Copyright &copy; Tafers 2017. All Rights Reserved</p>
     </div>
